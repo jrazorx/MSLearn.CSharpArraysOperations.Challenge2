7033100986b5030acccd8b6cecdcd9a1ebbafa8d
jra <julien.gault.50@gmail.com>
2024-03-06 18:19:05 +0100

    Add project files.

---
+++ MSLearn.CSharpArraysOperations.Challenge2/Program.cs
@@ -0,0 +1,26 @@
+﻿string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+
+/*
+Notice in the previous code, the orderStream variable contains a string of multiple Order IDs separated by commas
+
+Add code below the previous code to parse the "Order IDs" from the string of incoming orders and store the "Order IDs" in an array
+
+Add code to output each "Order ID" in sorted order and tag orders that aren't exactly four characters in length as "- Error"
+
+Save and run your code
+
+Your code must produce the following output:
+
+Output
+
+Copy
+A345
+B123
+B177
+B179
+C15     - Error
+C234
+C235
+G3003   - Error
+*/
+
